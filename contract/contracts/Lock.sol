447ab7caf3c9b58bb820d295df6d32411bbfbc11
shikhar360 <sssonishikhar360@gmail.com>
2023-02-13 16:37:55 +0530

    contract initialised

--- contract/contracts/Lock.sol
+++ contract/contracts/Lock.sol
@@ -1,34 +1,89 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+// import {IFakeDAI} from "./IFakeDAI.sol";
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import { ISuperfluid, ISuperToken } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+import {IConstantFlowAgreementV1} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
 
-    event Withdrawal(uint amount, uint when);
+import {CFAv1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
+// For deployment on Goerli Testnet
+contract FlowSender {
+
+    using CFAv1Library for CFAv1Library.InitData;
+    CFAv1Library.InitData public cfaV1; //initialize cfaV1 variable
+    
+    mapping (address => bool) public accountList;
+
+    ISuperToken public goerliDaiX;
+
+    // Host address on Goerli = 0x22ff293e14F1EC3A09B137e9e06084AFd63adDF9
+    // fDAIx address on Goerli = 0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00
+    constructor(ISuperfluid _host, ISuperToken _goerliDaiX) {
+
+        //initialize InitData struct, and set equal to cfaV1        
+        cfaV1 = CFAv1Library.InitData(
+            _host,
+            //here, we are deriving the address of the CFA using the host contract
+            IConstantFlowAgreementV1(
+                address(_host.getAgreementClass(
+                        keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1")
+                    ))
+            )
         );
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+        goerliDaiX = _goerliDaiX;
+
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    /// @dev Mints 10,000 fDAI to this contract and wraps it all into fDAIx
+    function gainDaiX() external {
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+        // Get address of fDAI by getting underlying token address from DAIx token
+        // IFakeDAI fdai = IFakeDAI( goerliDaiX.getUnderlyingToken() );
+        
+        // Mint 10,000 fDAI
+        // fdai.mint(address(this), 10000e18);
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+        // // Approve fDAIx contract to spend fDAI
+        // fdai.approve(address(goerliDaiX), 20000e18);
 
-        owner.transfer(address(this).balance);
+        // // Wrap the fDAI into fDAIx
+        // goerliDaiX.upgrade(10000e18);
+
     }
+
+    /// @dev creates a stream from this contract to desired receiver at desired rate
+    function createStream(int96 flowRate, address receiver) external {
+
+        // Create stream
+        cfaV1.createFlow(receiver, goerliDaiX, flowRate);
+
+    }
+
+    /// @dev updates a stream from this contract to desired receiver to desired rate
+    function updateStream(int96 flowRate, address receiver) external {
+
+        // Update stream
+        cfaV1.updateFlow(receiver, goerliDaiX, flowRate);
+
+    }
+
+    /// @dev deletes a stream from this contract to desired receiver
+    function deleteStream(address receiver) external {
+
+        // Delete stream
+        cfaV1.deleteFlow(address(this), receiver, goerliDaiX);
+
+    }
+
+    /// @dev get flow rate between this contract to certain receiver
+    function readFlowRate(address receiver) external view returns (int96 flowRate) {
+        
+        // Get flow rate
+        (,flowRate,,) = cfaV1.cfa.getFlow(goerliDaiX, address(this), receiver);
+
+    }
+
 }
